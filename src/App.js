e593be06b302c0eb3c486e75d4b699be0751ad0b
GharamAlessa <gharam.alessa@gmail.com>
2021-09-04 22:30:58 +0300

    lab02

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from "react";
-import Main from "./components/Main";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
+  
+import React from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+class App extends React.Component{
+render (){
+  return (
+    <>
+    <Header />
+    <Main/>
+    <Footer/>
+    </>
+  )
 }
-
-export default App;
+}
+export default App 
