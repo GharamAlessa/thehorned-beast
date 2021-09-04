d40133aca1edf84c745639cb50f67745aef3dde7
GharamAlessa <gharam.alessa@gmail.com>
2021-09-04 15:19:05 +0300

    lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+           
+            <footer><h4> &copy; Gharam Alessa</h4></footer>
+        )
+    }
+}
+export default Footer;
