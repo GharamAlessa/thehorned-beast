22993151cdbed3b03382e04c959488dbc65d71a0
GharamAlessa <gharam.alessa@gmail.com>
2021-09-13 18:15:07 +0300

    finish04

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,23 @@
 import React from "react";
 import HornedBeasts from "./HornedBeast";
+import data from "../assets/data.json";
+import SelectForm from "./SelectForm";
+
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newData: data,
+    };
+  }
+  handle = (item) => {
+    this.setState({ newdata: item });
+    console.log("sam", item);
+  };
+
   render() {
-    let arr = this.props.bests.map(horn => {
+    let arr = this.state.newData.map((horn) => {
       return (
         <HornedBeasts
           title={horn.title}
@@ -12,7 +27,12 @@ class Main extends React.Component {
         />
       );
     });
-    return <main>{arr}</main>;
+    return (
+      <main>
+        <SelectForm Callback={this.handle} />
+        {arr}
+      </main>
+    );
   }
 }
 
